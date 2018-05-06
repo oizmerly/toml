d833f167561fcce298ab34448be3bea4b0a55f37
Oleg Izmerly <oizmerly@splunk.com>
2018-05-06 10:39:10 -0700

    init

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"./toml"
+	"fmt"
 )
 
 var blob = `
@@ -13,10 +14,17 @@ yyy = 222
 aaa = 111
 bbb = true
 
+# comment 
+
 [stanza3]
 list = xxx
 `
 
 func main()  {
-	toml.Decode(blob)
+	data, err := toml.Decode(blob)
+	fmt.Println(data)
+	fmt.Println(err)
+
+	enc := toml.Encode(data)
+	fmt.Println(enc)
 }
--- toml/toml.go
+++ toml/toml.go
@@ -1,13 +1,54 @@
 package toml
 
-import "strings"
+import (
+	"strings"
+	"regexp"
+	"errors"
+)
 
-func Decode(data string) (interface{}, error)  {
-	var result interface{}
+type (
+	Stanza map[string]string
+	Data map[string]Stanza
+)
 
-	for _ := range strings.Split(data) {
+var ( // possible expressions
+	stanzaExpr = regexp.MustCompile(`^\s*\[(.*)\]\s*$`)
+	keyValueExpr = regexp.MustCompile(`^\s*(\w*)\s*=\s*(.*)\s*$`)
+	commentExpr = regexp.MustCompile(`^\s*#.*$`)
+)
 
+func Decode(input string) (Data, error)  {
+	result := make(Data)
+	var stanza Stanza
+
+	for _, line := range strings.Split(input, "\n") {
+		if match := stanzaExpr.FindStringSubmatch(line); len(match) > 0 {
+			stanza = make(map[string]string)
+			result[match[1]] = stanza
+		} else if match := keyValueExpr.FindStringSubmatch(line); len(match) > 0 {
+			if stanza == nil {
+				return nil, errors.New("key-value pair without stanza: " + line)
+			}
+			stanza[match[1]] = match[2]
+		} else if commentExpr.MatchString(line) {
+			// a comment - do nothing
+		} else {
+			errors.New("unknown expression: " + line)
+		}
 	}
 
 	return result, nil
+}
+
+func Encode(data Data) string  {
+	var result string
+
+	for stanza, content := range data {
+		result += "[" + stanza +"]\n"
+		for key, value := range content {
+			result += key + "=" + value + "\n"
+		}
+	}
+
+	return result
 }
